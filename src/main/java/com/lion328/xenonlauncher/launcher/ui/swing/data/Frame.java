a688a92a8a2e9bde0238a55861fcad3ca835055c
Waritnan Sookbuntherng <lion328@hotmail.co.th>
2016-05-13 22:13:45 +0700

    Added missing panel constructor and added iconURL field in Frame

--- src/main/java/com/lion328/xenonlauncher/launcher/ui/swing/data/Frame.java
+++ src/main/java/com/lion328/xenonlauncher/launcher/ui/swing/data/Frame.java
@@ -3,6 +3,8 @@ package com.lion328.xenonlauncher.launcher.ui.swing.data;
 import com.google.gson.annotations.SerializedName;
 import com.lion328.xenonlauncher.launcher.ui.swing.data.type.ComponentTypeAdapter;
 
+import java.net.URL;
+
 public class Frame extends Component
 {
 
@@ -19,18 +21,22 @@ public class Frame extends Component
     private boolean decorated = true;
     @SerializedName("panel")
     private Panel panel;
+    @SerializedName("iconURL")
+    private URL iconURL;
 
-    public Frame(String id, int width, int height, int x, int y, int color, boolean resizable, boolean decorated)
+    public Frame(String id, int width, int height, int x, int y, int color, boolean resizable, boolean decorated, Panel panel, URL iconURL)
     {
-        this(id, TYPE, width, height, x, y, color, resizable, decorated);
+        this(id, TYPE, width, height, x, y, color, resizable, decorated, panel, iconURL);
     }
 
-    public Frame(String id, String type, int width, int height, int x, int y, int color, boolean resizable, boolean decorated)
+    public Frame(String id, String type, int width, int height, int x, int y, int color, boolean resizable, boolean decorated, Panel panel, URL iconURL)
     {
         super(id, type, width, height, x, y, color);
 
         this.resizable = resizable;
         this.decorated = decorated;
+        this.panel = panel;
+        this.iconURL = iconURL;
     }
 
     public boolean isResizable()
@@ -46,5 +52,10 @@ public class Frame extends Component
     public Panel getPanel()
     {
         return panel;
+    }
+
+    public URL getIconURL()
+    {
+        return iconURL;
     }
 }
