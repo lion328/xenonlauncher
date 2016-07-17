200bba9fc5a7b5a83693a705a531c2e52fa0db4b
Waritnan Sookbuntherng <lion328@hotmail.co.th>
2016-07-17 18:54:24 +0700

    Reformat code

--- src/main/java/com/lion328/xenonlauncher/downloader/URLFileDownloader.java
+++ src/main/java/com/lion328/xenonlauncher/downloader/URLFileDownloader.java
@@ -7,7 +7,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
