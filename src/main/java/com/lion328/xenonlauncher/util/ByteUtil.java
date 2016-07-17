4db19a83a7cf944c74269d7c27235282d3474394
Waritnan Sookbuntherng <lion328@hotmail.co.th>
2016-07-17 11:04:04 +0700

    Fix uppercase hexadecimal not working in ByteUtil.fromHexString

--- src/main/java/com/lion328/xenonlauncher/util/ByteUtil.java
+++ src/main/java/com/lion328/xenonlauncher/util/ByteUtil.java
@@ -5,12 +5,16 @@ public class ByteUtil
 
     public static byte[] fromHexString(String s)
     {
+        s = s.toLowerCase();
+
         int len = s.length();
         byte[] data = new byte[len / 2];
+
         for (int i = 0; i < len; i += 2)
         {
             data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
         }
+
         return data;
     }
 }
